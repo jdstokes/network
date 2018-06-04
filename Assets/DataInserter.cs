d1b39eb69ab59a3dd54ba376d4bbb2dcacbf8d80
Jared Stokes <stokesjd@gmail.com>
2018-06-04 15:05:11 -0700

    update for online hosting

---
+++ Assets/DataInserter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class DataInserter : MonoBehaviour
+{
+
+    public string inputUserName;
+    public string inputPassword;
+    public string inputEmail;
+
+    //private string CreateUserURL = "http://localhost/Cool_YT_RPG/InsertUser.php";
+
+    private string CreateUserURL = "https://stokesjd.000webhostapp.com/InsertUser.php";
+
+    void Start () {
+
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(CreateUser(inputUserName, inputPassword, inputEmail));
+            Debug.Log("create user");
+        }
+    }
+	
+
+    public IEnumerator CreateUser(string username, string email, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost",username);
+        form.AddField("emailPost", email);
+        form.AddField("passwordPost", password);
+
+        WWW www = new WWW(CreateUserURL, form);
+        yield return www;
+        Debug.Log(www.text);
+
+
+    }
+}
---
+++ Assets/Login.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+
+    public string inputUserName = "jaredUnity";
+    public string inputPassword = "123456";
+//    private string LoginURL = "http://localhost/Cool_YT_RPG/Login.php";
+    private string LoginURL = "https://stokesjd.000webhostapp.com/InsertUser.php";
+
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.L)) StartCoroutine(LoginToDB(inputUserName, inputPassword));
+
+    }
+
+    IEnumerator LoginToDB(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("passwordPost", password);
+
+        WWW www = new WWW(LoginURL, form);
+
+        yield return www;
+
+        Debug.Log(www.text);
+    }
+}
